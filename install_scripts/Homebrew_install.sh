8bcdd68b4cd3dcd9bf18ec04c8ffe0ab31641627
Joseph Raetano <joseph.raetano@woz-u.com>
2021-07-30 18:11:08 -0400

    added

--- install_scripts/Homebrew_install.sh
+++ install_scripts/Homebrew_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this script is for mac and runs in a terminal and can run on latest M1 chip macs :) 
-# chmod 755 bash_install_v2.sh then ./bash_install_v2.sh
+# chmod 755 Homebrew_install.sh then ./Homebrew_install.sh
 
 #install homebrew
 echo "installing homebrew please enter your password when asked"
@@ -12,7 +12,7 @@ echo "installing python3"
 brew install python3
 
 #install r
-echo "installing R"
+echo "installing R App"
 brew install --cask r
 
 #install rstudio
@@ -43,7 +43,7 @@ brew services start mysql
 echo "installing mongosh"
 brew install mongosh
 
-#todo: install mongo
+# todo: install mongo
 # brew tap mongodb/brew
 # brew install mongodb-community@4.2
 # brew services start mongodb-community@4.2
@@ -67,5 +67,8 @@ brew install --cask zoom
 #install tableau
 echo "installing tableau enter password when asked"
 brew install --cask tableau-public
+
+#optional anaconda3 install
+# brew install --cask anaconda
 
 
