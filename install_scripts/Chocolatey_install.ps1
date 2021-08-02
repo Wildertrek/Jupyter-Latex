7dd9fe7a893087c56da62be234933e7212b181cf
Joseph Raetano <joseph.raetano@woz-u.com>
2021-08-02 18:10:58 -0400

    updated and cleared up confusion

--- install_scripts/Chocolatey_install.ps1
+++ install_scripts/Chocolatey_install.ps1
@@ -1,10 +1,12 @@
+#step 0: ensure you have a solid, high-bandwidth and unlimited 
+# connection to the internet
 
-# Step 1:
+# Step 1: if you have already run step2 in a previous session, skip this step and move on to step2
 # Open Powershell as Administrator
 Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope CurrentUser 
 #<Press A> for All
 
-# Step 2: Run this script or command to install Chocolately in Powershell
+# Step 2: FIRST RUN!!! Run this script or command to install Chocolately in Powershell run as Administrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Step 3: Reopen Powershell for chocolately to take effect
@@ -13,4 +15,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco install slack zoom python mysql mysql-python mysql.workbench mongodb r r.studio vscode tableau-public github-desktop anaconda3 speedtest 
 #<Press A> for All
 
-#reboot
+# Step 5: reboot your computer
